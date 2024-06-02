17081c189875bb6aa8be1448ab923ff0f4d253ca
rajmishra1089 <rajmishra1089@gmail.com>
2024-06-03 00:36:07 +0530

    Add files via upload

---
+++ client/api/index.js
@@ -0,0 +1,4 @@
+import axios from './axiosReq';
+import { USER } from './requests'
+
+export {USER,axios};
---
+++ client/api/requests.js
@@ -0,0 +1,10 @@
+const USER = {
+    getTranscript: "/transcripts/getTrans",
+    anyPrompt:"/gen/anyPrompt",
+    summarize:"/gen/summarize",
+    quiz:"/gen/quiz",
+    langChange:'/gen/langChange',
+    checkAns:'/gen/checkAns'
+  };
+
+export {USER};
