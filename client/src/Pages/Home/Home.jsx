5ae0453f894ca976d1f47b09d30adb6e811df1ca
rajmishra1089 <rajmishra1089@gmail.com>
2024-06-03 00:42:04 +0530

    Create Home.jsx

---
+++ client/src/Pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../../components/Navbar";
+import Moto_Link from "../../components/Moto_Link";
+
+
+export default function Home() {
+  return (
+    <div>
+      <Navbar />
+      <Moto_Link />
+    </div>
+  );
+}
